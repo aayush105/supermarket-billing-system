1bd6379777c0cc5e8a7d0dcd63e6848045ef61dd
Aayush shrestha <115408327+aayush105@users.noreply.github.com>
2023-08-10 19:58:10 +0545

    Update DB.java

--- src/com/db/DB.java
+++ src/com/db/DB.java
@@ -9,7 +9,7 @@ public class DB {
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306/marketDB","root","Aayush_105");
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/<Database Name>","<username>","<password>");
 			
 		} catch (Exception e) {
 			
